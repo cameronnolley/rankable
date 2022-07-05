e4f094724eae21345cedb9f2e1328f5fa0719851
Cameron Nolley <cameronnolley@gmail.com>
2022-07-05 10:38:44 -0500

    Updates

---
+++ precache-manifest.4dfe4ceaf3621621e53a87d8b5d443c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c1ffd535695e68a4487",
+    "url": "/rankable/static/css/main.20b157d5.chunk.css"
+  },
+  {
+    "revision": "4c1ffd535695e68a4487",
+    "url": "/rankable/static/js/main.4c1ffd53.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/rankable/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "67f045a7b0d8113a2bb4",
+    "url": "/rankable/static/js/2.67f045a7.chunk.js"
+  },
+  {
+    "revision": "135cdfd1d9786a3c3305",
+    "url": "/rankable/static/js/runtime~main.135cdfd1.js"
+  },
+  {
+    "revision": "d0e637663b97038f57c7deed9978901f",
+    "url": "/rankable/index.html"
+  }
+];
