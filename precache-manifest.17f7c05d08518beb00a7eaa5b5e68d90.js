6e87b007a7baec64bed4f019f0f531953a122db8
Cameron Nolley <cameronnolley@gmail.com>
2022-07-21 16:56:54 -0500

    Updates

--- precache-manifest.17f7c05d08518beb00a7eaa5b5e68d90.js
+++ precache-manifest.17f7c05d08518beb00a7eaa5b5e68d90.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "87b81f24cd2827b03c23",
-    "url": "/rankable/static/css/main.a77d5d09.chunk.css"
+    "revision": "a8e0915c1ec8b3ee0e48",
+    "url": "/rankable/static/css/main.efddf480.chunk.css"
   },
   {
-    "revision": "87b81f24cd2827b03c23",
-    "url": "/rankable/static/js/main.87b81f24.chunk.js"
+    "revision": "a8e0915c1ec8b3ee0e48",
+    "url": "/rankable/static/js/main.a8e0915c.chunk.js"
   },
   {
     "revision": "3a354c580f07f251fff3",
     "url": "/rankable/static/js/1.3a354c58.chunk.js"
   },
   {
-    "revision": "fae43b66c36cb8cec881",
-    "url": "/rankable/static/js/2.fae43b66.chunk.js"
+    "revision": "df8b7493136c79cff329",
+    "url": "/rankable/static/js/2.df8b7493.chunk.js"
   },
   {
     "revision": "ce4786afc6e3f86a3bd9",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/rankable/static/media/rankabl-logo.251c7ee4.png"
   },
   {
-    "revision": "3615ade13c44b768070f7be50c155232",
+    "revision": "e2cd3db3e2bb1e109f1d18d92ff31374",
     "url": "/rankable/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rankable/precache-manifest.73b552daa6fd419fec5535fc6c663cfc.js"
+  "/rankable/precache-manifest.17f7c05d08518beb00a7eaa5b5e68d90.js"
 );
 
 workbox.clientsClaim();
